b15653c17132c0b626e1ca8de30a1d4d94b4662c
FLAVIA DE ALMEIDA GUTERRES <70322589215@jpmorgan.sp>
2017-11-29 19:41:14 -0200

    Adding Follow-twitter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Hero from "./components/Hero/Hero";
+import Follow-twitter from "./components/Follow-twitter/Follow-twitter";
 
 export default () => (
   <div className="App">
     <Hero />
+    <Follow-twitter />
   </div>
 );
---
+++ src/components/Follow-twitter/Follow-twitter.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Follow-twitter.css"
+
+export default (props) => (
+    <div className="follow-twitter">
+        <p className="follow-twitter-paragraph">
+            Follow along on Twitter for all the latest news! <a href="">@Progress_App</a>
+        </p>
+    </div>
+);
