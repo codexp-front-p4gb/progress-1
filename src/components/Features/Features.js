ef3c30cc34e48fdf3520427aaccab3391cdba9cf
31275376894 <31275376894@info.local>
2017-11-29 20:17:29 -0200

    mudanças

--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Extrato from "./extrato.png";
 import Protocolo from "./protocolo.png";
+import Extrato from "./extrato.png";
 import "./Features.css"
 
 export default (props) => (
@@ -14,7 +14,7 @@ export default (props) => (
                         quisquam illo dolores minus numquam rem libero expedita saepe!</p>
                 </div>
                             <figure className="feature__picture">
-                                <img src={Protocolo} alt="Screen mobile"/>
+                                <img src={Protocolo} alt="Protocolo"/>
                             </figure>
                 
                 <div className="feature__content">
@@ -22,20 +22,18 @@ export default (props) => (
                         <p className="feature__content-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem nam eum officia fuga eius accusantium placeat,
                         quisquam illo dolores minus numquam rem libero expedita saepe!</p>
                 </div>
-
             </div>
-                  <div className="feature feature-reverse">
-                    
-                    <figure className="feature__picture">
+            <div className="feature feature-reverse">
+                <figure className="feature__picture">
                         <img src={Extrato} alt="Extrato contrato"/>
-                    </figure>
+                </figure>
 
-                    <div className="feature__content">
-                        <h2 className="feature__content-title"> Estimate, approve, pay</h2>
+                <div className="feature__content">
+                    <h2 className="feature__content-title"> Estimate, approve, pay</h2>
                             <p className="feature__content-description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem nam eum officia fuga eius accusantium placeat,
                             quisquam illo dolores minus numquam rem libero expedita saepe!</p>
-                    </div>
-                 </div>
+                </div>
+            </div>
     </section>
 </main>
 );
