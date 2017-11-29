6c19c26bf8b30847df38f720cf652d24f7e4993a
MARA TAMIRIS MIRANDA DE OLIVEIRA <43901448802@jpmorgan.sp>
2017-11-29 20:28:34 -0200

    Changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Hero from "./components/Hero/Hero";
+import FlexiblePlataform from "./components/FlexiblePlataform/FlexiblePlataform";
 
 export default () => (
   <div className="App">
     <Hero />
+    <FlexiblePlataform />
   </div>
 );
---
+++ src/components/FlexiblePlataform/FlexiblePlataform.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "FlexiblePlataform.css"
+
+export default (props) => (
+    <section className="flexible-plataform">
+        <h1 className="flexible-plataform__titulo" >Flexible Plataform</h1>
+        <p className="flexible-plataform__texto" >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis commodi illum ipsam nihil, nisi iste iusto esse consectetur sed et impedit! Vitae expedita sunt sapiente illum sint rerum, vero at!</p>
+        <input className="flexible-plataform__input" name="email" type="email" placeholder="  Add your email address"/>
+        <button className="flexible-plataform__botao" >invited me</button>
+    </section>
+);
