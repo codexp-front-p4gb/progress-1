a664b3e47dc15e4a336f0839c7547594d02d4c7a
RODRIGO LUIZ DE OLIVEIRA <40666540888@jpmorgan.sp>
2017-11-29 21:49:48 -0200

    uniãodos componentes  no APPS.JS

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import Hero from "./components/Hero/Hero";
 import Features from "./components/Features/Features";
-
+import BringYourOwnBusiness from  "./components/BringYourOwnBusiness/BringYourOwnBusiness";
 import FlexiblePlataform from "./components/FlexiblePlataform/FlexiblePlataform";
-
-import BringYourOwnBusiness from "./components/BringYourOwnBusiness/BringYourOwnBusiness";
-
 import FollowTwitter from "./components/FollowTwitter/FollowTwitter";
-
-
-import 
-
+import Footer from "./components/Footer/Footer";
 
 export default () => (
   <div className="App">
     <Hero />
-
+    <Features />
+    <BringYourOwnBusiness />
+    <FlexiblePlataform />
+    <FollowTwitter />
+    <Footer />
   </div>
 );
 
