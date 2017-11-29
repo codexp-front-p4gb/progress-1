ff387c1ee1fc2b2e7315ce7ad962617dc8dfdfa4
GABRIELA BRAVO PEREIRA <38522701890@jpmorgan.sp>
2017-11-29 20:18:03 -0200

    bring your ownbusiness js ready for test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Hero from "./components/Hero/Hero";
+import BringYourOwnBusiness from "./components/BringYourOwnBusiness/BringYourOwnBusiness";
 
 export default () => (
   <div className="App">
     <Hero />
+    <BringYourOwnBusiness />
   </div>
 );
