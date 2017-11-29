53ea82d60e3d954b8d799884d2bad9018909c2c3
FLAVIA DE ALMEIDA GUTERRES <70322589215@jpmorgan.sp>
2017-11-29 19:44:20 -0200

    teste

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import Hero from "./components/Hero/Hero";
-import Follow-twitter from "./components/Follow-twitter/Follow-twitter";
 
 export default () => (
   <div className="App">
     <Hero />
-    <Follow-twitter />
   </div>
 );
